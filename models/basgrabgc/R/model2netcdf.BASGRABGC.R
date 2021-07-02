43eb52563ad9207fcf07e114d8fca4890091d222
Julius Vira <julius.vira@fmi.fi>
2021-07-02 20:50:09 +0300

    add model2netcdf script for basgra-bgc

--- models/basgrabgc/R/model2netcdf.BASGRABGC.R
+++ models/basgrabgc/R/model2netcdf.BASGRABGC.R
@@ -21,17 +21,118 @@
 ##' @export
 ##'
 ##' @author Rob Kooper
-model2netcdf.BASGRABGC <- function(outdir, sitelat, sitelon, start_date, end_date) {
-  PEcAn.logger::logger.severe("NOT IMPLEMENTED")
 
-  # Please follow the PEcAn style guide:
-  # https://pecanproject.github.io/pecan-documentation/develop/coding-style.html
-  
-  # Note that `library()` calls should _never_ appear here; instead, put
-  # packages dependencies in the DESCRIPTION file, under "Imports:".
-  # Calls to dependent packages should use a double colon, e.g.
-  #    `packageName::functionName()`.
-  # Also, `require()` should be used only when a package dependency is truly
-  # optional. In this case, put the package name under "Suggests:" in DESCRIPTION. 
-  
-} # model2netcdf.BASGRABGC
+die <- PEcAn.logger::logger.severe
+
+make_netcdf <- function(df_output, df_info, nc_path, sitelat, sitelon) {
+  df_info_act <- df_info[df_info$name.bethy != '', ]
+  year <- df_output[1, 'year']
+
+  if (any(df_output[,'year'] != year)) {
+    PEcAn.logger::logger.severe('Only one year per file allowed')
+  }
+
+  timedim <- ncdf4::ncdim_def('time',
+                              units = sprintf('days since %i-01-01', year),
+                              vals = df_output[,'doy']-1,
+                              calendar = 'standard',
+                              unlim = TRUE)
+  latdim <- ncdf4::ncdim_def('lat', 'degrees_north', vals = as.numeric(sitelat), longname = 'station_latitude')
+  londim <- ncdf4::ncdim_def('lon', 'degrees_east', vals = as.numeric(sitelon), longname = 'station_longitude')
+
+  var_lst <- list()
+  dims = list(lon = londim, lat = latdim, time = timedim)
+  # declare variables
+  for (ind_var in seq_len(nrow(df_info_act))) {
+    var_lst[[ind_var]] <- ncdf4::ncvar_def(df_info_act[ind_var, 'name.bethy'],
+                                           units = df_info_act[ind_var, 'unit.bethy'],
+                                           dim = dims,
+                                           missval = -999,
+                                           longname = df_info_act[ind_var, 'long_name'])
+  }
+
+  nc <- ncdf4::nc_create(nc_path, var_lst)
+  varfile <- file(sprintf('%s.var', nc_path), 'w')
+  # write variables
+  for (ind_var in seq_along(var_lst)) {
+    ncdf4::ncvar_put(nc, var_lst[[ind_var]], df_output[, var_lst[[ind_var]]$name])
+    cat(paste(var_lst[[ind_var]]$name, var_lst[[ind_var]]$longname), file = varfile, sep = "\n")
+  }
+  close(varfile)
+  ncdf4::nc_close(nc)
+}
+
+split_by_year <- function(df) {
+  years <- seq(df[1, 'year'], df[nrow(df), 'year'])
+  lapply(years, function(year) df[df$year == year,])
+}
+
+process_output <- function(df.raw, df.info) {
+  varnames <- df.info[df.info$name.basgra != '', 'name.basgra']
+  if (ncol(df.raw) != length(varnames) + 3) {
+    PEcAn.logger::logger.severe('Wrong number of columns in output file')
+  }
+  colnames(df.raw) <- c('decimal_year', 'year', 'doy', varnames)
+
+  vars2extract <- df.info[df.info$name.bethy != '',]
+  convert_ind <- function(ind_row) {
+    if (vars2extract[ind_row, 'name.bethy'] == 'NEE') {
+      # below, convert g m-2 day-1 to kg m-2 s-1
+      result <- (df.raw[,'ECORES'] - df.raw[,'GPP']) * 1e-3 / (24*3600)
+    } else if (vars2extract[ind_row, 'name.basgra'] != '') {
+      data.raw <- df.raw[,vars2extract[ind_row, 'name.basgra']]
+      conv <- vars2extract[ind_row, 'conv2bethy']
+      if (is.na(conv)) PEcAn.logger::logger.severe(sprintf('Cannot unit-convert bethy variable: %s', vars2extract[ind_row, 'name.bethy']))
+      result <- data.raw*conv
+    } else {
+      PEcAn.logger::logger.severe(sprintf('Cannot convert bethy variable: %s', vars2extract[ind_row, 'name.bethy']))
+    }
+    return(result)
+  }
+  bethy_lst <- lapply(seq_len(nrow(vars2extract)), convert_ind)
+  names(bethy_lst) <- vars2extract$name.bethy
+  bethy_lst[['year']] <- df.raw[,'year']
+  bethy_lst[['doy']] <- df.raw[,'doy']
+  df.processed <- as.data.frame(bethy_lst)
+  split_by_year(df.processed)
+}
+
+
+
+date_from_year_doy <- function(year, doy) {
+  lubridate::ymd(sprintf('%i-01-01', year)) + lubridate::days(doy-1)
+}
+
+get_model2netcdf <- function(make_netcdf_func, get_output_func, get_out_info_func) {
+  func <- function(outdir, sitelat, sitelon, start_date, end_date) {
+    df_info <- get_out_info_func()
+    output_path <- file.path(outdir, "basgrabgc.out") # we assume that run.id is included in outdir
+    output.raw <- get_output_func(output_path)
+    output <- process_output(output.raw, df_info) # list(df_year1, df_year2, ...)
+    num_years <- length(output)
+    
+    # no subsetting but check that the start, end dates are equal
+    first_date_read <- date_from_year_doy(output[[1]][1, 'year'], output[[1]][1, 'doy'])
+    num_days_last_yr <- nrow(output[[num_years]])
+    last_date_read <- date_from_year_doy(output[[num_years]][num_days_last_yr, 'year'], output[[num_years]][num_days_last_yr, 'doy'])
+
+    if (first_date_read != start_date) {
+      PEcAn.logger::logger.severe(sprintf('start_date mismatch: %s <> %s', as.character(first_date_read), as.character(start_date)))
+    } else if (last_date_read != end_date) {
+      PEcAn.logger::logger.severe(sprintf('end_date mismatch: %s <> %s', as.character(last_date_read), as.character(end_date)))
+    }
+
+    for (ind_yr in seq_len(num_years)) {
+      yr <- lubridate::year(start_date) + ind_yr - 1
+      nc_path <- file.path(outdir, sprintf('%i.nc', yr))
+      make_netcdf_func(output[[ind_yr]], df_info, nc_path, sitelat, sitelon)
+    }
+  }
+}
+
+model2netcdf.BASGRABGC <- get_model2netcdf(make_netcdf,
+                                           get_output_func = function(filepath) {
+                                             read.table(file_path)},
+                                           get_out_info_func = function() {
+                                             read.csv(system.file('basgra-bgc.output.map.csv', package='PEcAn.BASGRABGC'))}
+                                           )
---
+++ models/basgrabgc/tests/testthat/test.model2netcdf.R
@@ -0,0 +1,127 @@
+context("model2netcdf")
+
+tmpdir <- tempfile()
+setup(dir.create(tmpdir, showWarnings = FALSE))
+teardown(unlink(tmpdir, recursive = TRUE))
+
+df.test <- data.frame(dyear = seq(2021, 2023, length=3*365),
+                      year = c(rep(2021, 365), rep(2022, 365), rep(2023, 365)) ,
+                      doy = rep(seq(365), 3),
+                      ECORES = c(rep(1, 365), rep(2, 365), rep(3, 365)),
+                      GPP = rep(99, 3*365))
+df.info <- data.frame(name.basgra = c('ECORES', 'GPP', ''),
+                      long_name = c('Eco Res', 'Gross Peanut Production', 'Net Ecosystem Exchange'),
+                      name.bethy = c('', 'GPP', 'NEE'),
+                      unit.basgra = c('g m-2 d-1', 'peanuts s-1', ''),
+                      unit.bethy = c('', 'p s-1', 'kg m-2 s-1'),
+                      conv2bethy = c(NA, 10, NA))
+
+test_that('process_output works', {
+  years <- c(2021, 2022, 2023)
+  output <- process_output(df.test, df.info)
+  expect_equal(length(output), 3) # three years
+  for (ind_yr in seq_len(3)) {
+    expect_setequal(output[[ind_yr]]$year, years[ind_yr])
+    expect_setequal(output[[ind_yr]]$GPP, 99*10) # conv2bethy = 10
+    expect_equal(output[[ind_yr]]$NEE, # see above how the values are defined
+                 udunits2::ud.convert(ind_yr - rep(99, 365), 'g m-2 d-1', 'kg m-2 s-1'))
+    expect_equal(nrow(output[[ind_yr]]), 365)
+    expect_false(any(is.na(output[[ind_yr]])))
+  }
+  expect_error(process_output(df.test, df.info[,1:4]))
+})
+
+test_that('model2netcdf date checking logic works', {
+  stub_func <- get_model2netcdf(make_netcdf_func = function(...){},
+                                get_output_func = function(...) { df.test[1:365,] },
+                                get_out_info_func = function() { df.info })
+  expect_silent(stub_func('', NA, NA, lubridate::ymd('2021-01-01'), lubridate::ymd('2021-12-31')))
+  expect_error(stub_func('', NA, NA, lubridate::ymd('2020-01-01'), lubridate::ymd('2021-12-31')))
+  expect_error(stub_func('', NA, NA, lubridate::ymd('2021-01-01'), lubridate::ymd('2024-01-01')))
+})
+
+test_that('netcdf files are requested to be created as expected', {
+  my.sitelat <- 1.0
+  my.sitelon <- 9.124
+  my.info <- df.info
+  my.output <- process_output(df.test, df.info)
+  files_created <- list()
+  check_netcdf <- function(df_output, df_info, nc_path, sitelat, sitelon) {
+    # this function checks that make_netcdf is called with right arguments and records
+    # the files that would be created
+    year <- df_output[1,'year']
+    name_expected <- file.path(tmpdir, sprintf('%i.nc', year))
+    expect_equal(nc_path, name_expected)
+    expect_equal(sitelat, my.sitelat)
+    expect_equal(sitelon, my.sitelon)
+    expect_equal(df_info, my.info)
+    files_created[[length(files_created)+1]] <<- nc_path
+  }
+  stub_func <- get_model2netcdf(make_netcdf_func = check_netcdf,
+                                get_output_func = function(...) df.test,
+                                get_out_info_func = function() df.info)
+  start_date <- lubridate::ymd('2021-01-01')
+  end_date <- lubridate::ymd('2023-12-31')
+  stub_func(tmpdir, my.sitelat, my.sitelon, start_date, end_date)
+  expect_equal(files_created, list(file.path(tmpdir, '2021.nc'), file.path(tmpdir, '2022.nc'), file.path(tmpdir, '2023.nc')))
+})
+
+test_that('make_netcdf writes what is required', {
+  df <- process_output(head(df.test), df.info)[[1]]
+  nc_path <- file.path(tmpdir, 'test.nc')
+  sitelat <- 90.0
+  sitelon <- 180.0
+  make_netcdf(df, df.info, nc_path, sitelat = sitelat, sitelon = sitelon)
+  expect_true(file.exists(nc_path))
+
+  nc <- ncdf4::nc_open(nc_path)
+  time_units <- ncdf4::ncatt_get(nc, 'time', 'units')$value
+  expect_equal(time_units, 'days since 2021-01-01')
+  # in the following I want the attributes dropped thus c(ncdf4::ncvar_get(...))
+  times <- c(ncdf4::ncvar_get(nc, 'time'))
+  expect_equal(times, df$doy-1)
+  expect_equal(c(ncdf4::ncvar_get(nc, 'lat')), sitelat)
+  expect_equal(c(ncdf4::ncvar_get(nc, 'lon')), sitelon)
+  expect_equal(c(ncdf4::ncvar_get(nc, 'GPP')), df[, 'GPP'])
+  expect_equal(c(ncdf4::ncvar_get(nc, 'NEE')), df[, 'NEE'])
+  expect_equal(ncdf4::ncatt_get(nc, 'GPP', 'long_name')$value, df.info[df.info$name.bethy == 'GPP', 'long_name'])
+  expect_equal(ncdf4::ncatt_get(nc, 'NEE', 'long_name')$value, df.info[df.info$name.bethy == 'NEE', 'long_name'])
+  expect_equal(ncdf4::ncatt_get(nc, 'GPP', 'units')$value, df.info[df.info$name.bethy == 'GPP', 'unit.bethy'])
+  expect_equal(ncdf4::ncatt_get(nc, 'NEE', 'units')$value, df.info[df.info$name.bethy == 'NEE', 'unit.bethy'])
+  ncdf4::nc_close(nc)
+  # check the varfile
+  var_table <- read.table(sprintf('%s.var', nc_path))
+  expect_equal(var_table[,1], df.info$name.bethy[df.info$name.bethy != ''])
+  # the following should hold but doesn't because there are (legitimately) spaces in the long_names.
+  # expect_equal(var_table[,2], df.info$long_name[df.info$name.bethy != ''])
+})
+
+test_that('met2model passes with real input', {
+  func2file <- get_model2netcdf(make_netcdf,
+                                get_output_func = function(ignored) {
+                                  read.table('test.output')},
+                                get_out_info_func = function() {
+                                  read.csv(system.file('basgra-bgc.output.map.csv', package='PEcAn.BASGRABGC'))}
+                                )
+  sitelat <- 0.0
+  sitelon <- 0.1
+  start_date <- lubridate::ymd('2020-05-01')
+  end_date <- lubridate::ymd('2020-05-19')
+  expect_silent(func2file(tmpdir, sitelat, sitelon, start_date, end_date))
+  # capture the output sent to netcdf
+  outputs <- list()
+  capture_output <- function(df_output, df_info, nc_path, sitelat, sitelon) {
+    outputs[[length(outputs)+1]] <<- df_output
+  }
+  func2lst <- get_model2netcdf(capture_output,
+                               get_output_func = function(ignored) {
+                                 read.table('test.output')},
+                               get_out_info_func = function() {
+                                  read.csv(system.file('basgra-bgc.output.map.csv', package='PEcAn.BASGRABGC'))}
+                               )
+  func2lst(tmpdir, sitelat, sitelon, start_date, end_date)
+  expect_length(outputs, 1)
+  expect_false(any(is.na(outputs[[1]])))
+  expect_equal(nrow(outputs[[1]]), 19)
+  expect_equal(outputs[[1]][19, 'leaf_carbon_content'], 13.46275*1e-3)
+})
